a286c99c3edd6557cf5bf76346a754d4aac80fb9
Abhishek Vijay <Abhishek Vijay>
2017-03-29 12:09:32 +0530

    added template fetching

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require('https');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -26,6 +27,44 @@ app.get('/:name', function (req, res, next) {
   });
 
 });
+
+app.get('/template/:email', function(req, res, next){
+  
+    if(req.headers["Authorization"] != "hktemplatepass")
+    {
+        res.statusCode = 403;
+        res.end();
+    }
+
+    var options = {
+    "method": "GET",
+    "hostname": "raw.githubusercontent.com",
+    "port": null,
+    "path": "/higherknowledge/outlook-integration/master/templates/srivalli%2540higherknowledge.in",
+    "headers": {
+      "content-type": "application/json",
+      "cache-control": "no-cache",
+      "postman-token": "140da308-7068-ff29-82ff-4bc2ec3e39fe"
+    }
+  };
+
+    var gitReq = http.request(options, function (gitRes) {
+      var chunks = [];
+
+      gitRes.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      gitRes.on("end", function () {
+        var body = Buffer.concat(chunks);
+        res.write(body.toString());
+        res.end();
+      });
+    });
+    gitReq.end();
+  }
+  
+);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
