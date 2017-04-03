a920690cf891c570d8db1b0d0028fba347eb2e6c
b-abv <b-abv@microsoft.com>
2017-04-03 16:21:24 +0530

    added cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('https');
 app.set('port', (process.env.PORT || 5000));
 
 
-app.options('/', function(req, res, next)
+app.options('/*', function(req, res, next)
 {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
