b98288d6cec4c789acf73f939b81ac75e517e8e0
Abhishek V <abhivijay96@gmail.com>
2017-11-03 18:44:28 +0530

    debug

--- addin/app.js
+++ addin/app.js
@@ -13,7 +13,28 @@
       $("#intern").click(handleIntern);
       $("#recruit").click(handleRecruit);
     });
+
+    window.onerror = function (msg, url, lineNo, columnNo, error) {
+      var string = msg.toLowerCase();
+      var substring = "script error";
+      if (string.indexOf(substring) > -1){
+          //alert('Script Error: See Browser Console for Detail');
+          debug.innerHTML = "Script Error";
+      } 
+      else {
+          var message = [
+              'Message: ' + msg,
+              'URL: ' + url,
+              'Line: ' + lineNo,
+              'Column: ' + columnNo,
+              'Error object: ' + JSON.stringify(error)
+          ].join(' - ');
+          document.getElementById("debug").innerHTML = JSON.stringify(message, '\t');
+      }
+
+      return false;
   };
+};
 
   function run() {
 
